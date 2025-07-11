4171e20b899097fce6448f1ca4b61a3eb87bd3f0
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-07-11 11:40:27 +0800

    fix user index village not needed in migration

--- database/migrations/ngpam_000001_create_users_table.php
+++ database/migrations/ngpam_000001_create_users_table.php
@@ -22,8 +22,6 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
-
-            $table->index(['village_id', 'is_active']);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
